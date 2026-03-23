b6ddf1e94bb3b9f28f3eb0995cb2a46237e6465d
Lokesh Kumar <lokeshk24@iitk.ac.in>
2026-03-23 12:11:37 +0530

    better way to find anagrams

--- Phase_1_Basic_Python_Logic/05_are_anagrams/anagram_check_for_two_Strings.py
+++ Phase_1_Basic_Python_Logic/05_are_anagrams/anagram_check_for_two_Strings.py
@@ -2,19 +2,20 @@ def are_anagrams(s1, s2):
     if len(s1) != len(s2):
         return False
     
-    counter_s1 = {}
-    counter_s2 = {}
+    counter = {}
 
     for char in s1:
-        if char in counter_s1:
-            counter_s1[char] += 1
+        if char in counter:
+            counter[char] += 1
         else:
-            counter_s1[char] = 1
+            counter[char] = 1
 
     for char in s2:
-        if char in counter_s2:
-            counter_s2[char] += 1
-        else:
-            counter_s2[char] = 1
+        if char not in counter:
+            return False
+        counter[char] -= 1
 
-    return counter_s1 == counter_s2
+    for value in counter.values():
+        if value != 0:
+            return False
+    return True
