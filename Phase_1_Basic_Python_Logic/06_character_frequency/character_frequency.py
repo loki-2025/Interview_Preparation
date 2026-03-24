64926cfd3779ea594cb121cf60f57766cd680ffa
Lokesh Kumar <lokeshk24@iitk.ac.in>
2026-03-24 14:03:06 +0530

    added comments

--- Phase_1_Basic_Python_Logic/06_character_frequency/character_frequency.py
+++ Phase_1_Basic_Python_Logic/06_character_frequency/character_frequency.py
@@ -1,9 +1,17 @@
 def character_frequency(string):
+    """
+    This function calculates frequency of the character in a string
+    
+    Args:
+        param1 (str): Input String
+    Returns:
+        dict: Mapping of character to its frequency count
+    """
     counter = {}
     for char in string:
         if char in counter:
             counter[char] += 1
         else:
-            counter[char] =1
+            counter[char] = 1
     
     return counter
